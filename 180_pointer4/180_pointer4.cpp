c180a2550a1c5974dd93f10766767737e3a965b6
dzulfaqaar <fikri6aul@gmail.com>
2023-05-08 16:03:34 +0700

    done

--- 180_pointer4/180_pointer4.cpp
+++ 180_pointer4/180_pointer4.cpp
@@ -15,5 +15,18 @@ public:
 		nilai = 100;
 	}
 	friend void manusia::showNilaiPelajar(pelajar& x);
-
 };
+
+void manusia::showNilaiPelajar(pelajar& x) {
+	cout << "Nilai Pelajar: " << x.nilai;
+}
+int main()
+{
+	manusia m;
+	pelajar p;
+	m.showNilaiPelajar(p);
+	cout << endl;
+	system("\npause");
+	return 0;
+
+}
